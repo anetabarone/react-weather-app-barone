e1903d4fd8ff424c3243c8322df00fd69276439e
anetabarone <anetabarone@yahoo.com>
2023-05-14 18:07:22 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is created by Aneta Barone and is{" "}
-        <a
-          href="https://github.com/anetabarone/react-weather-app-barone"
-          target="blank"
-        >
-          open-sourced on GitHub
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This project is created by Aneta Barone and is{" "}
+          <a
+            href="https://github.com/anetabarone/react-weather-app-barone"
+            target="blank"
+          >
+            open-sourced on GitHub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
