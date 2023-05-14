e41260261ac791dfb5d38d812c9736525854b8b9
anetabarone <anetabarone@yahoo.com>
2023-05-14 16:28:56 +0200

    added link to GitHub

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is created by Aneta Barone and is{" "}
+        <a
+          href="https://github.com/anetabarone/react-weather-app-barone"
+          target="blank"
+        >
+          open-sourced on GitHub
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
 
 export default App;
