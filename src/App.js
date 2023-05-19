ee9a6ec57f7b1c9969b8f49731ac92147993a3b2
anetabarone <anetabarone@yahoo.com>
2023-05-19 20:28:13 +0200

    API calls implemented

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
-        <Weather />
+        <Weather defaultCity="Berlin" />
         <footer>
           This project is created by Aneta Barone and is{" "}
           <a
