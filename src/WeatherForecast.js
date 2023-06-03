2d79049964957164c3480c6ddf4857233e94161e
anetabarone <anetabarone@yahoo.com>
2023-06-03 19:09:01 +0200

    added function load()

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,6 +17,16 @@ export default function WeatherForecast(props) {
     setLoaded(true);
   }
 
+  function load() {
+    let apiKey = "63214c4281922e3bb72fdf12dada7734";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
+
   if (loaded) {
     console.log(forecast);
     return (
@@ -37,12 +47,6 @@ export default function WeatherForecast(props) {
       </div>
     );
   } else {
-    let apiKey = "63214c4281922e3bb72fdf12dada7734";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleResponse);
-    return null;
+    load();
   }
 }
